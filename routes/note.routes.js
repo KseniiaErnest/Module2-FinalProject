4da9039392f3301cac47bd1791b3c773e943d904
Kseniia Ernest <hramtcovakseniia@gmail.com>
2023-06-12 19:45:16 -0400

    CRUD for Note model complete

--- routes/note.routes.js
+++ routes/note.routes.js
@@ -30,7 +30,7 @@ router.post('/create-note/:flashcardId', /* isLoggedIn , */ (req, res, next) => 
     })
     .then(() => {
       req.flash('success', 'Note created successfully');
-      res.redirect(`/flashCards/flashCard-details/${flashcardId}`);
+      res.redirect(`/flashCards/details/${flashcardId}`);
     })
     .catch((error) => {
       next(error);
@@ -42,6 +42,63 @@ router.post('/create-note/:flashcardId', /* isLoggedIn , */ (req, res, next) => 
 
   
 });
+
+// Display All Notes for the FlashCard
+router.get('/see-notes/:flashcardID',  /* isLoggedIn , */ (req, res, next) => {
+  const flashCardID = req.params.flashcardID;
+
+  Note.find({ $and: [{belongToCard: flashCardID}, {composedByUser: req.session.currentUser._id}]})
+  .then((notes) => {
+    res.render('notes/notes-list', { notes });
+  })
+  .catch((error) => {
+    next(error);
+  })
+});
+
+// Update Note GET and POST route
+router.get('/edit-note/:id', /* isLoggedIn , */ (req, res, next) => {
+  Note.findById(req.params.id)
+  .then((theNote) => {
+    res.render('notes/note-edit', { theNote });
+  })
+  .catch((error) => {
+    next(error);
+  })
+});
+
+router.post('/update-note/:theID', /* isLoggedIn , */ (req, res, next) =>{
+
+  Note.findByIdAndUpdate(req.params.theID, {
+    title: req.body.theTitle,
+    content: req.body.theContent
+  })
+  .then(() => {
+    Note.findById(req.params.theID)
+    .then((updatedNote) => {
+      res.redirect(`/note/see-notes/${updatedNote.belongToCard}`)
+    })
+    .catch((error) => {
+      next(error);
+    });
+  })
+  .catch((error) => {
+    next(error);
+  })
+});
+
+// Delete Note POST route
+router.post('/:id/delete-note', /* isLoggedIn , */ (req, res, next) => {
+Note.findByIdAndDelete(req.params.id)
+.then((deletedNote) => {
+const flashCardId = deletedNote.belongToCard;
+req.flash('success', 'Note deleted successfully');
+res.redirect(`/note/see-notes/${flashCardId}`);
+})
+.catch((error) => {
+  next(error);
+})
+})
 
 
 
