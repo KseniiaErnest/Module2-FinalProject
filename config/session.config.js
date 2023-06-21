ecb36aa02c508122e1517b7d269209f38ba150c1
Kseniia Ernest <hramtcovakseniia@gmail.com>
2023-06-20 22:35:36 -0400

    Working on CSS mobile, checking for bugs

--- config/session.config.js
+++ config/session.config.js
@@ -19,7 +19,7 @@ module.exports = app => {
         maxAge: 600000
       },
       store: MongoStore.create({
-        mongoUrl: process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/Module2-FinalProject'
+        mongoUrl: process.env.MONGODB_URI
       })
     })
   );
